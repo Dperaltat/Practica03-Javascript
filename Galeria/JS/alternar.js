5528e989c19d4ab57e96f37b96672add1b42ceb0
Dperaltat <peraltadaniel069@gmail.com>
2019-05-19 00:39:12 -0500

    Arreglaod JS

--- Galeria/JS/alternar.js
+++ Galeria/JS/alternar.js
@@ -6,12 +6,12 @@ function carrousel(contenedor){
     contenedor.addEventListener('click', e => {
         let atras = contenedor.querySelector('.atras'),
         adelante = contenedor.querySelector('.adelante'),
-        img = contenedor.querySelector('Imagenes'),
+        img = contenedor.querySelector('img'),
         tgt = e.target;
 
         if(tgt==atras){
             if(cont>0){
-                img.src = imagenes[conta - 1];
+                img.src = imagenes[cont - 1];
                 cont--;
             }else{
                 img.src = imagenes[imagenes.length - 1];
@@ -19,7 +19,7 @@ function carrousel(contenedor){
             }
         }else if(tgt == adelante){
             if(cont < imagenes.length -1){
-                img.src = imagenes[conta + 1];
+                img.src = imagenes[cont + 1];
                 cont++;
             }else{
                 img.src = imagenes[0];
